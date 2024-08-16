7783aadcf24841edcd2cfd2812fdef1119ab4be1
AvdeevK <marselo1910@gmail.com>
2024-08-16 23:58:27 +0300

    fixed default values

--- internal/config/config.go
+++ internal/config/config.go
@@ -13,8 +13,8 @@ func ParseFlags() {
 
 	// регистрируем переменную flagRunAddr
 	// как аргумент -a со значением :8080 по умолчанию
-	flag.StringVar(&Configs.RequestAddress, "a", ":8080", "server listening port")
-	flag.StringVar(&Configs.ResponseAddress, "b", ":8080", "url availiable at port")
+	flag.StringVar(&Configs.RequestAddress, "a", "localhost:8080", "server listening port")
+	flag.StringVar(&Configs.ResponseAddress, "b", "http://localhost:8080", "url availiable at port")
 	// парсим переданные серверу аргументы в зарегистрированные переменные
 	flag.Parse()
 }
